b4fedf6718ff70b4632fe9bff49165f91ac7464e
abulhussian12 <abulhussian1229@gmail.com>
2024-10-06 11:45:37 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+        white:'#FFFFFF',
+        grey:'#333333',
+        black:'#141414',
+        Darkgrey:'#1F1F1F',
+        neongreen:'#C4F82A'
+      }
+    },
+  },
+  plugins: [],
+}
